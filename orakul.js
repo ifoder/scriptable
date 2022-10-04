a9b9dcfc591510ba7cb7005a09d10aa36181936a
FlexDev Team <weflexdev@gmail.com>
2022-10-04 23:15:00 +0200

    second

--- orakul.js
+++ orakul.js
@@ -1,62 +1,93 @@
-let url = "https://orakul.com/horoscope/astrologic/general/aries/today.html";
-let req = new Request(url);
-let html = await req.loadString();
+const gradientTop = "#16213E";
+const gradientBottom = "#0F3460";
+const textColor = "#ffffff";
+
+const headingText = "Общий гороскоп на\n сегодня, ";
+const formatDat = "d MMMM";
+
+/*
+ * * Change the value to whatever
+ * * you need from 0 to 11.
+ * * Where 0 corresponds to "Aries"
+ * * and so on. You can also set this
+ * * value to a widget setting, set Parameter inputt
+ * */
+let zodiac = 0;
+
+const zodiacArr = [
+  ["♈ Овен", "aries"], // 0
+  ["♉ Телец", "taurus"], // 1..
+  ["♊ Близнеци", "gemini"],
+  ["♋ Рак", "cancer"],
+  ["♌ Лев", "lion"],
+  ["♍ Дева", "virgo"],
+  ["♎ Весы", "libra"],
+  ["♏ Скорпион", "scorpio"],
+  ["♐ Стрелец", "sagittarius"],
+  ["♑ Козерог", "capricorn"],
+  ["♒ Водолей", "aquarius"],
+  ["♓ Рыбы", "piscec"], // 11
+];
+
+const inputParameters = args.widgetParameter;
+if (inputParameters) zodiac = inputParameters;
+
+const url =
+  "https://orakul.com/horoscope/astrologic/general/" +
+  zodiacArr[zodiac][1] +
+  "/today.html";
 
 const widget = new ListWidget();
 
-async function CreateWidget() {
-  let tex = extract(html);
+async function buildWidget() {
+  const contentText = await orakulParse();
 
-  //  Add background gradient
-  let gradient = new LinearGradient();
+  const gradient = new LinearGradient();
   gradient.locations = [0, 1];
-
-  gradient.colors = [new Color("141414"), new Color("13233F")];
-
+  gradient.colors = [new Color(gradientTop), new Color(gradientBottom)];
   widget.backgroundGradient = gradient;
 
-  let headingS = widget.addStack();
-  let contentS = widget.addStack();
+  const headingStack = widget.addStack();
+  const contentStack = widget.addStack();
+  headingStack.setPadding(0, 0, 10, 10);
 
-  headingS.setPadding(0, 0, 10, 10);
-
-  let headingLeft = headingS.addStack();
-  let headingRight = headingS.addStack();
-
+  const headingLeft = headingStack.addStack();
+  const headingRight = headingStack.addStack();
+  headingRight.setPadding(0, 0, 0, 0);
   headingRight.addSpacer(80);
-  let heading = headingLeft.addText("♉️ Овен");
 
+  const heading = headingLeft.addText(zodiacArr[zodiac][0]);
   heading.font = Font.mediumSystemFont(18);
   heading.textOpacity = 0.9;
-  heading.textColor = new Color("#ffffff");
+  heading.textColor = new Color(textColor);
 
-  let d = new DateFormatter();
-  d.dateFormat = "dd MMMM";
+  const dateSet = new DateFormatter();
+  dateSet.dateFormat = formatDat;
+  const date = dateSet.string(new Date());
 
-  let date = d.string(new Date());
-  log(date);
-
-  let headingDate = headingRight.addText(date);
-  headingDate.font = Font.mediumSystemFont(14);
-  headingDate.setPadding(10, 0, 0, 0);
-  headingDate.textOpacity = 0.9;
+  const headingDate = headingRight.addText(headingText + date);
+  headingDate.font = Font.mediumSystemFont(10);
+  headingDate.textOpacity = 0.8;
   headingDate.rightAlignText();
-  headingDate.textColor = new Color("#ffffff");
+  headingDate.textColor = new Color(textColor);
 
-  let content = contentS.addText(tex);
+  const content = contentStack.addText(contentText);
   content.font = Font.lightSystemFont(12);
   content.textOpacity = 0.7;
-  content.textColor = new Color("#ffffff");
-  log(tex);
+  content.textColor = new Color(textColor);
 }
-await CreateWidget();
-function extract(html) {
-  let start = html.indexOf("horoBlock");
-  let end = html.indexOf("</p>", start + 1);
-  let prognoz = html.substring(start + 41, end);
 
-  return prognoz;
+async function orakulParse() {
+  const req = new Request(url);
+  const request = await req.loadString();
+
+  const start = request.indexOf("horoBlock");
+  const end = request.indexOf("</p>", start + 1);
+
+  return request.substring(start + 41, end);
 }
+
+await buildWidget();
 
 if (config.runsInWidget) {
   Script.setWidget(widget);
